c2c325899845e115ab522f32e860f3098b7f4a6c
sidi.lekhalifa <sidi@carizy.com>
2024-01-04 14:19:11 +0100

    [LAST] : Add new library confetti for test include using asset-mapper

--- assets/app.js
+++ assets/app.js
@@ -6,4 +6,15 @@
  */
 import './styles/app.css'
 
-console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉')
+import JSConfetti from 'js-confetti';
+
+const jsConfetti = new JSConfetti();
+
+jsConfetti.addConfetti({
+    emojis: ['🌈', '⚡️', '💥', '✨', '💫', '🌸'],
+})
+jsConfetti.addConfetti({
+    confettiColors: [
+        '#ff0a54', '#ff477e', '#ff7096', '#ff85a1', '#fbb1bd', '#f9bec7',
+    ],
+})
